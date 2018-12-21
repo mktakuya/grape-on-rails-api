b93a9cf9728136b5f92dd7989d75f9b1bcc8f962
Takuya Mukohira <takuya.mk96@gmail.com>
2018-12-22 00:07:15 +0900

    Add auth helpers

--- app/api/v1/root.rb
+++ app/api/v1/root.rb
@@ -3,6 +3,20 @@ module V1
     version :v1
     format :json
 
+    TOKEN_PREFIX = /^Bearer\s/
+    TOKEN_REGEX = /^Bearer\s(.+)/
+    helpers do
+      def authenticate!
+        error!('Unauthorized. Invalid or expired token.', 401) unless current_user
+      end
+
+      def current_user
+        return nil unless headers['Authorization'] && headers['Authorization'].match(TOKEN_REGEX)
+        token = headers['Authorization'].gsub(TOKEN_PREFIX, '')
+        User.find_by(token: token)
+      end
+    end
+
     mount V1::Users
     mount V1::Authors
     mount V1::Books
