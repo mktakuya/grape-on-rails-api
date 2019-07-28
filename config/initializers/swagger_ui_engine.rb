ed60bc0487e1032f3d3025bb894c350102aafbab
Takuya Mukohira <takuya.mk96@gmail.com>
2019-07-28 23:57:58 +0900

    Setup SwaggerUIEngine

---
+++ config/initializers/swagger_ui_engine.rb
@@ -0,0 +1,5 @@
+SwaggerUiEngine.configure do |config|
+  config.swagger_url = {
+    v1: '/v1/swagger_doc'
+  }
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   mount Base::API => '/'
+
+  mount SwaggerUiEngine::Engine, at: '/v1/docs'
 end
